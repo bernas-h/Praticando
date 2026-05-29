f3fa9a16b4658625d29134cecacf511dcc61efc7
Bernardo Henrique <bernardo08bh@gmail.com>
2026-05-29 15:26:21 -0300

    array com for

--- array.js
+++ array.js
@@ -9,3 +9,13 @@ arr[0] = 10;
 
 console.log(arr);
 
+function criaArr(inicio, fim){
+    const arr = [];
+    let elemento = inicio;
+    for(let i = 0, j=0; j < fim; i++, elemento++){
+        arr[i] = elemento;
+        j=elemento;
+    }
+    return arr;
+}
+    console.log(criaArr(400, 454));
